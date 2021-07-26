3df440a7907a3c00d94859afbe66c156a527dbe4
thanhtd32 <53929436+thanhtd32@users.noreply.github.com>
2021-07-27 05:22:59 +0900

    Add files via upload

---
+++ Exercise11.py
@@ -0,0 +1,69 @@
+#Coder: Tran Duy Thanh
+#Email: thanhtd@uel.edu.vn
+#Phone: 0987773061
+#Blog for self-study: https://duythanhcse.wordpress.com
+#Facebook for solving coding problem: https://www.facebook.com/groups/communityuni
+
+value=0
+while True:
+    print("\nCurrent value:",value)
+    line=input("Enter the work command:")
+    tokens=line.split()
+    if len(tokens)>0:
+        operator=tokens[0]
+        if len(tokens) ==1:
+            if operator =='x':
+                break
+            print("Wrong work order!!")
+        else:
+            listOperand=[]#a list to store all operands
+            isAllOperandValid=True
+            # loop to store all operands into listOperand
+            for i in range(1,len(tokens)):
+                if tokens[i].isdigit() == False:
+                    isAllOperandValid=False
+                    break
+                operand = float(tokens[i])
+                listOperand.append(operand)
+            if isAllOperandValid == True:
+                if operator== '=':
+                    # get the last operand to asign for value
+                    value=listOperand[len(listOperand)-1]
+                elif operator=='+':
+                    # loop and plus all operand into value variable
+                    for operand in listOperand:
+                        value+=operand;
+                elif operator=='-':
+                    # loop and minus all operand into value variable
+                    for operand in listOperand:
+                        value-=operand;
+                elif operator=='*':
+                    # loop and multiply all operand into value variable
+                    for operand in listOperand:
+                        value*= operand;
+                elif operator=='^':#loop and exponential all operand into value variable
+                    for operand in listOperand:
+                        value=pow(value,operand)
+                elif operator=='/' or operator=='%':
+                    checkDividedZore=False
+                    for operand in listOperand:  # loop to check diveded by zero
+                        if operand==0:
+                            checkDividedZore=True
+                            break
+                    if checkDividedZore==False:
+                        if operator=='/':
+                            # loop and devide all operand into value variable
+                            for operand in listOperand:
+                                value /= operand;
+                        else:
+                            # loop and mod all operand into value variable
+                            for operand in listOperand:
+                                value %= operand;
+                    else:
+                        print("Illegal operation command (divided by zero)!!")
+                else:
+                    print("Operator is not exist![=,+,-,*,/,%,^]")
+            else:
+                print("Some operand is not valid!")
+    else:
+        print("Invalid string input format")
