8ef2a98a0cd5200f19f8f8832b948cff7a643489
thanhtd32 <53929436+thanhtd32@users.noreply.github.com>
2023-09-16 14:27:11 +0700

    Update Exercise23.py

--- Exercise23.py
+++ Exercise23.py
@@ -1,8 +1,8 @@
 #Coder: Tran Duy Thanh
 #Email: thanhtd@uel.edu.vn
 #Phone: 0987773061
-#Blog for self-study: https://duythanhcse.wordpress.com
-#Facebook for solving coding problem: https://www.facebook.com/groups/communityuni
+#Blog for self-study: https://tranduythanh.com
+
 
 #Description:
     #These codes I improved from Exercise 22.
